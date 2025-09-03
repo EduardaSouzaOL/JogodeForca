b55ece22c0a9dd2f31ceb93dd4df62351da0ef82
EduardaSouzaOL <messtudies18@gmail.com>
2025-09-03 12:29:15 -0300

    varre o array, e procura letra

---
+++ jogo_da_forca.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char palavrasecreta[20];
+    sprintf(palavrasecreta, "MELANCIA");
+ 
+
+    int acertou = 0;
+    int enforcou = 0;
+    do
+    {
+        // chute do usúario
+        char chute;
+        scanf("%c", &chute);
+        //varre o array e verifica quais posições da letra chutada
+        for (int i = 0; i < strlen(palavrasecreta); i++)
+        {
+            if (palavrasecreta[i] == chute)
+            {
+                printf("A posição %d tem essa letra\n", i);
+            }
+        }
+        // enquanto não acertou e não enforcou
+    } while (!acertou && !enforcou);
+ 
+}
