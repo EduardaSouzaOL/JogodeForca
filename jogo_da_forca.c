fa4e8aa32e45b4b20e03deeacb6f8a71e2d30bba
EduardaSouzaOL <messtudies18@gmail.com>
2025-09-03 15:40:26 -0300

    jogo formatado da maneira correeta, sem a visualização do lopping

--- jogo_da_forca.c
+++ jogo_da_forca.c
@@ -10,23 +10,18 @@ int main()
 
     char chutes[26];
     int tentativas = 0;
+
     do
     {
         // tracejado
-        for (int i = 0; i < strlen(palavrasecreta); i++)
+        for (int i = 0; i < (strlen(palavrasecreta)); i++)
         {
-
             int achou = 0;
-
-            printf("Estou vendo a letra secreta %d  = %c\n", i, palavrasecreta[i]);
 
             for (int j = 0; j < tentativas; j++)
             {
-
-                printf("-> Chute %d = %c\n", j, chutes[j]);
                 if (chutes[j] == palavrasecreta[i])
                 {
-                    printf("---> chute correto!\n");
                     achou = 1;
                     break;
                 }
